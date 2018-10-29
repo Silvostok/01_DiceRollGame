d5e58f89105c778c771a030c818c9568f7c6b984
Miguel Angel Rubio Caballero <Miguel.Rubio.Caballero@hva.nl>
2018-10-29 20:55:48 +0100

    fix images

--- app/src/main/java/com/example/miguelangelrubiocaballero/a01_dicerollgame/MainActivity.java
+++ app/src/main/java/com/example/miguelangelrubiocaballero/a01_dicerollgame/MainActivity.java
@@ -72,20 +72,6 @@ public class MainActivity extends AppCompatActivity {
 
         mListView.setAdapter(mAdapter);
 
-
-//------------
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-
-        //------------
     }
 
 
@@ -118,10 +104,6 @@ public class MainActivity extends AppCompatActivity {
             highscore = 0;
             higscoreCounter = 0;
         }
-
-
-
-
 
 
 
